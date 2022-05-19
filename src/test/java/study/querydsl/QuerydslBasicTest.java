638efe5efb1eeb086d0cefd9602c119dc190934c
choisoobong <devcsb119@gmail.com>
2022-05-19 17:07:58 +0900

    기본문법 - 조인 - 기본조인
    기본 조인 사용법, 세타 조인 사용법 숙지, 세타조인에 대한 이해, 세타조인 사용시 on 절 사용없이는 외부조인(left outer, right outer)을 사용할 수 없음을 이해.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -272,6 +272,48 @@ public class QuerydslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35); // (30 + 40) / 2
+    }
+
+    /**
+     * 팀 A에 소속된 모든 회원을 찾기
+     */
+    @Test
+    public void join() throws Exception{
+        //given
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)  // join(조인 대상, 별칭으로 사용할 Q타입)  // leftJoin(), rightJoin() 가능
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        //then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     * 외부 조인 불가능 -> 조인 on을 사용하면 외부 조인 가능
+     */
+    @Test
+    public void theta_join() throws Exception{
+        //given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        //when
+        List<Member> result = queryFactory
+                .select(member)  // from 절에 여러 엔티티를 선택해서 세타 조인
+                .from(member, team) // 모든 member와 모든 team을 가져와서 where절에서 필터링하는 것이라 이해하면 된다. (물론 db가 성능최적화를 해준다.)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        //then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
 
     }
 
